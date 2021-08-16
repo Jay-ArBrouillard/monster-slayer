664a82a65962cc86c3b36864bb30a05e6be7174d
Jay-Ar Brouillard <jay-ar.brouillard@flexagon.com>
2021-08-16 14:04:04 -0500

    Improve ui, add strength to calculation, Fix special attack bug, fix Monster heal after game end bug

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = Vue.createApp({
     mounted: function () {
         // Heal Monster if his heal is low enough
         window.setInterval(() => {
-            if (this.goblin.monsterHp <= Math.floor(this.goblin.maxHp / 4)) {
+            if (this.goblin.monsterHp > 0 && this.goblin.monsterHp <= Math.floor(this.goblin.maxHp / 4)) {
                 if (this.goblin.monsterHp + 1 > this.goblin.maxHp) {
                     this.goblin.monsterHp = this.goblin.maxHp
                 } else {
@@ -36,13 +36,13 @@ const app = Vue.createApp({
                 name: 'Hero',
                 heroHp: 100,
                 maxHp: 100,
-                strength: 5
+                strength: 8
             },
             goblin: {
                 name: 'Goblin',
                 monsterHp: 200,
                 maxHp: 200,
-                strength: 10
+                strength: 6
             }
         }
     },
@@ -80,7 +80,7 @@ const app = Vue.createApp({
                 this.addLog('Monster', 'stunned', `stunned for ${this.monsterStunnedCountDown} more turn(s)`)
             } else {
                 //Monster will always have 100% accuracy
-                let monsterAttack = getRandomInRange(6, 12) 
+                let monsterAttack = getRandomInRange(this.goblin.strength, this.goblin.strength * 2) 
                 //If you fail a stun, then monster will counter attack for big damage
                 if (this.battleLogs[0].message === 'attempted to stun Monster and failed') {
                     monsterAttack *= 2.5
@@ -100,19 +100,19 @@ const app = Vue.createApp({
             const accuracy = this.calculateAccuracy(this.heroAttackStyle, this.monsterAttackStyle)
             const hit = accuracy >= getRandomInRange(1, 100)
             if (hit) {
-                let heroAttack = getRandomInRange(8, 15) 
+                let heroAttack = getRandomInRange(this.hero.strength, this.hero.strength*2) 
                 //Boost attack
                 if (accuracy === 100) heroAttack = Math.ceil(heroAttack * 1.5)
                 this.goblin.monsterHp -= heroAttack
-                if (this.usedSpecialAttack) this.currentRound++;
                 this.addLog('Hero', 'attack', `${this.heroAttackStyle} attacked and dealt`, heroAttack)
             } else {
                 this.addLog('Hero', 'miss', `attempted ${this.heroAttackStyle} attack and missed`)
             }
+            if (this.usedSpecialAttack) this.currentRound++;
             this.attackHero()
         },
         specialAttackMonster() {
-            const heroSpecialAttack = getRandomInRange(12, 25) 
+            const heroSpecialAttack = getRandomInRange(this.hero.strength*2, this.hero.strength*3) 
             this.goblin.monsterHp -= heroSpecialAttack
             this.usedSpecialAttack = true
             this.currentRound++;
@@ -122,10 +122,10 @@ const app = Vue.createApp({
         heal() {
             const healValue = getRandomInRange(3, 20)
             if (healValue + this.hero.heroHp > 100) {
-                this.goblin.heroHp = 100
+                this.hero.heroHp = 100
                 this.addLog('Hero', 'fullHeal', 'fully heals to', healValue)
             } else {
-                this.goblin.heroHp += healValue
+                this.hero.heroHp += healValue
                 this.addLog('Hero', 'heal', 'heals himself for', healValue)
             }
             if (this.usedSpecialAttack) this.currentRound++;
@@ -140,6 +140,7 @@ const app = Vue.createApp({
                 this.addLog('Monster', 'stunFail', 'attempted to stun Monster and failed')
                 this.attackHero()
             }
+            if (this.usedSpecialAttack) this.currentRound++;
         },
         reset() {
             this.hero.heroHp = this.hero.maxHp;
@@ -167,6 +168,16 @@ const app = Vue.createApp({
     },
     computed: {
         monsterHealthBarValue() {
+            if (this.goblin.monsterHp < 0) return 0
+            else if (this.goblin.monsterHp === this.goblin.maxHp) return this.goblin.maxHp
+            return Math.floor(this.goblin.monsterHp / this.goblin.maxHp * this.goblin.maxHp)
+        },
+        heroHealthBarValue() {
+            if (this.hero.heroHp < 0) return 0
+            else if (this.hero.heroHp === this.hero.maxHp) return this.hero.maxHp
+            return Math.floor(this.hero.heroHp / this.hero.maxHp * this.hero.maxHp)
+        }, 
+        monsterHealthBarStyle() {
             if (this.goblin.monsterHp < 0) {
                 return {width: '0%'}
             }
@@ -174,7 +185,7 @@ const app = Vue.createApp({
                 width: `${Math.floor(this.goblin.monsterHp / this.goblin.maxHp * 100)}%`
             }
         },
-        heroHealthBarValue() {
+        heroHealthBarStyle() {
             if (this.hero.heroHp < 0) {
                 return {width: '0%'}
             }
@@ -183,7 +194,8 @@ const app = Vue.createApp({
             }
         }, 
         specialAttackOnCooldown() {
-            return this.currentRound % 3 !== 0;
+            if (this.currentRound % 3 === 0) this.usedSpecialAttack = false
+            return this.usedSpecialAttack;
         },
         gameOver() {
             return this.hero.heroHp <= 0 || this.goblin.monsterHp <= 0

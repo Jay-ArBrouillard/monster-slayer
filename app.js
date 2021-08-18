037a6235eced0fa82f3b6da63f834bc39c32c169
Jay-Ar Brouillard <jay-ar.brouillard@flexagon.com>
2021-08-17 19:04:20 -0500

    Many changes

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
-function getRandomInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomNumberBetween(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+// 2 decimal rounded
+function round(num) {
+    return Math.round((num + Number.EPSILON) * 100) / 100;
 }
 
 const app = Vue.createApp({
     data() {
         return { 
+            currentRound: 1,
             toggleRules: false,
-            heroAccuracy: 100,
-            monsterAccuracy: 100,
             heroAttackStyle: '',
-            monsterAttackStyle: '',
-            monsterAttackStyleOrder: ['ranged', 'melee', 'melee', 'mage'],
-            currentRound: 0,
+            specialAttackCounter: 0,
+            specialAttackMinHit: 16,
+            specialAttackMaxHit: 24,
             usedSpecialAttack: false,
             battleLogs: [],
             monsterStunnedCountDown: 0,
-            idCounter: 0,
+            healCount: 10,
             hero: {
                 name: 'Hero',
                 heroHp: 100,
@@ -28,117 +34,187 @@ const app = Vue.createApp({
             },
             goblin: {
                 name: 'Goblin',
+                monsterHp: 100,
+                maxHp: 100,
+                strength: 5,
+                monsterAttackStyle: '',
+                monsterAttackStyleOrder: ['melee'],
+            },
+            barbarian: {
+                name: 'Barbarian',
+                monsterHp: 100,
+                maxHp: 100,
+                strength: 5,
+                baseStrength: 5,
+                monsterAttackStyle: '',
+                monsterAttackStyleOrder: ['melee', 'ranged'],
+            },
+            dracula: {
+                name: 'Dracula',
                 monsterHp: 200,
                 maxHp: 200,
-                strength: 6
-            }
+                strength: 9,
+                monsterAttackStyle: '',
+                monsterAttackStyleOrder: ['mage', 'melee'],
+            },
+            currentMonster: '',
         }
     },
     methods: {
-        calculateAccuracy(attackType, otherAttackType) {
+        calculateAttack(attackType, otherAttackType) {
             if (attackType === 'melee') {
                 if (otherAttackType === 'melee') {
-                    return 90;
+                    return {accuracy: 90, damageModifier: 1};
                 } else if (otherAttackType=== 'ranged') {
-                    return 100;
+                    return {accuracy: 100, damageModifier: 1.1};
                 } else {
-                    return 75;
+                    return {accuracy: 80, damageModifier: 0.9};
                 }
             } else if (attackType === 'ranged') {
                 if (otherAttackType === 'melee') {
-                    return 75;
+                    return {accuracy: 80, damageModifier: 0.9};
                 } else if (otherAttackType === 'ranged') {
-                    return 90;
+                    return {accuracy: 90, damageModifier: 1};
                 } else {
-                    return 100;
+                    return {accuracy: 100, damageModifier: 1.1};
                 }
             } else {
                 if (otherAttackType === 'melee') {
-                    return 100;
+                    return {accuracy: 100, damageModifier: 1.1};
                 } else if (otherAttackType === 'ranged') {
-                    return 75;
+                    return {accuracy: 80, damageModifier: 0.9};
                 } else {
-                    return 90;
+                    return {accuracy: 90, damageModifier: 1};
                 }
             }
         },
         attackHero() {
             if (this.monsterStunnedCountDown > 0) {
                 this.monsterStunnedCountDown--;
-                this.addLog('Monster', 'stunned', `stunned for ${this.monsterStunnedCountDown} more turn(s)`)
+                this.addLog(this.currentMonster.name, 'stunned', `stunned for ${this.monsterStunnedCountDown} more turn(s)`)
             } else {
-                //Monster will always have 100% accuracy
-                let monsterAttack = getRandomInRange(this.goblin.strength, this.goblin.strength * 2) 
-                //If you fail a stun, then monster will counter attack for big damage
-                if (this.battleLogs[0].message === 'attempted to stun Monster and failed') {
-                    monsterAttack = Math.ceil(monsterAttack * 2.5)
-                    this.addLog('Monster', 'attack', `${this.monsterAttackStyle} counter attacked and dealt`, monsterAttack)
+                if (this.currentMonster.name === 'Barbarian') this.calculateBarbarianStrength()
+                const attackObject = this.calculateAttack(this.currentMonster.monsterAttackStyle, this.heroAttackStyle)
+                const hit = attackObject.accuracy >= getRandomNumberBetween(1, 100)
+                if (hit) {
+                    let monsterAttack = round(getRandomNumberBetween(this.currentMonster.strength, this.currentMonster.strength * 2) * attackObject.damageModifier)
+                    //If you fail a stun, then monster will counter attack for big damage
+                    if (this.battleLogs[0].message === 'attempted to stun Monster and failed') {
+                        monsterAttack = round(monsterAttack * 2.5)
+                        this.addLog(this.currentMonster.name, 'attack', `${this.currentMonster.monsterAttackStyle} counter attacked and dealt`, monsterAttack)
+                    } else {
+                        this.addLog(this.currentMonster.name, 'attack', `${this.currentMonster.monsterAttackStyle} attacked and dealt`, monsterAttack)
+                    }
+                    this.hero.heroHp -= monsterAttack
+                    if (this.currentMonster.name === 'Dracula') {
+                        //Lifesteal
+                        if (getRandomNumberBetween(1,100) <= 10) {
+                            const lifestealAmount = Math.max(1, round(monsterAttack * 0.1)) //10% of attack with minimum of 1
+                            if (lifestealAmount + this.currentMonster.monsterHp < this.currentMonster.maxHp) {
+                                this.currentMonster.monsterHp += lifestealAmount
+                                this.addLog(this.currentMonster.name, 'other', `${this.currentMonster.name} healed ${lifestealAmount} via lifesteal`)
+                            } else {
+                                this.currentMonster.monsterHp = this.currentMonster.maxHp
+                                this.addLog(this.currentMonster.name, 'other', `${this.currentMonster.name} healed to full via lifesteal`)
+                            }
+                        }
+                    }
+    
+                    this.currentMonster.monsterAttackStyleOrder.push(this.currentMonster.monsterAttackStyleOrder.shift()) //rotate array
+                    this.currentMonster.monsterAttackStyle = this.currentMonster.monsterAttackStyleOrder[this.currentMonster.monsterAttackStyleOrder.length - 1]
                 } else {
-                    this.addLog('Monster', 'attack', `${this.monsterAttackStyle} attacked and dealt`, monsterAttack)
+                    this.addLog(this.currentMonster.name, 'miss', `attempted ${this.currentMonster.monsterAttackStyle} attack and missed`)
                 }
-                this.hero.heroHp -= monsterAttack
-
-                this.monsterAttackStyleOrder.push(this.monsterAttackStyleOrder.shift()) //rotate array
-                this.monsterAttackStyle = this.monsterAttackStyleOrder[3]
             }
         },
         attackMonster(type) {
             this.heroAttackStyle = type
-            this.monsterAttackStyle = this.monsterAttackStyleOrder[0]
-            const accuracy = this.calculateAccuracy(this.heroAttackStyle, this.monsterAttackStyle)
-            const hit = accuracy >= getRandomInRange(1, 100)
+            this.currentMonster.monsterAttackStyle = this.currentMonster.monsterAttackStyleOrder[0]
+            const attackObject = this.calculateAttack(this.heroAttackStyle, this.currentMonster.monsterAttackStyle)
+            const hit = attackObject.accuracy >= getRandomNumberBetween(1, 100)
             if (hit) {
-                let heroAttack = getRandomInRange(this.hero.strength, this.hero.strength*2) 
-                //Boost attack
-                if (accuracy === 100) heroAttack = Math.ceil(heroAttack * 1.5)
-                this.goblin.monsterHp -= heroAttack
+                let heroAttack = round(getRandomNumberBetween(this.hero.strength, this.hero.strength*2) * attackObject.damageModifier)
+                this.currentMonster.monsterHp -= heroAttack
                 this.addLog('Hero', 'attack', `${this.heroAttackStyle} attacked and dealt`, heroAttack)
             } else {
                 this.addLog('Hero', 'miss', `attempted ${this.heroAttackStyle} attack and missed`)
             }
-            if (this.usedSpecialAttack) this.currentRound++;
-            this.attackHero()
+            if (this.usedSpecialAttack) this.specialAttackCounter++;
+            if (this.currentMonster.monsterHp <= 0) {
+                this.addLog('Hero', 'kill', `slain ${this.currentMonster.name}. Round ${this.currentRound} completed`)
+                this.currentRound++
+                if (this.currentRound === 2) this.currentMonster = this.barbarian
+                else this.currentMonster = this.dracula
+            } else {
+                this.attackHero()
+            }
         },
         specialAttackMonster() {
-            const heroSpecialAttack = getRandomInRange(this.hero.strength*2, this.hero.strength*3) 
-            this.goblin.monsterHp -= heroSpecialAttack
+            const heroSpecialAttack = round(getRandomNumberBetween(this.specialAttackMinHit, this.specialAttackMaxHit) )
+            //Special attack always hits
+            this.currentMonster.monsterHp -= heroSpecialAttack
             this.usedSpecialAttack = true
-            this.currentRound++;
+            this.specialAttackCounter++;
             this.addLog('Hero', 'special', 'special attacked and deals', heroSpecialAttack)
+            if (this.currentMonster.monsterHp <= 0) {
+                this.addLog('Hero', 'kill', `slain ${this.currentMonster.name}. Round ${this.currentRound} completed`)
+                this.currentRound++
+                if (this.currentRound === 2) this.currentMonster = this.barbarian
+                else this.currentMonster = this.dracula
+            } else {
+                this.attackHero()
+            }
+            this.specialAttackMinHit = 16
+            this.specialAttackMaxHit = 24
+        },
+        chargeSpecialAttack() {
+            //Special attack always hits
+            this.specialAttackMinHit = Math.floor(this.specialAttackMinHit * (Math.max(1.1, Math.random() * 0.5 + 1)))
+            this.specialAttackMaxHit = Math.ceil(this.specialAttackMaxHit * (Math.max(1.1, Math.random() * 0.5 + 1)))
+            this.addLog('Hero', 'charge', `charged special attack to new max hit of ${this.specialAttackMaxHit}`)
             this.attackHero()
         },
         heal() {
-            const healValue = getRandomInRange(1, 20)
+            const missingHP = Math.ceil((this.hero.maxHp - this.hero.heroHp) * 0.5)
+            const healValue = getRandomNumberBetween(1, missingHP)
             if (healValue + this.hero.heroHp > 100) {
-                this.hero.heroHp = 100
-                this.addLog('Hero', 'fullHeal', 'fully heals to', healValue)
+                this.hero.heroHp = this.hero.maxHp
+                this.addLog('Hero', 'fullHeal', 'heals to full', healValue)
             } else {
                 this.hero.heroHp += healValue
                 this.addLog('Hero', 'heal', 'heals himself for', healValue)
             }
-            if (this.usedSpecialAttack) this.currentRound++;
+            if (this.usedSpecialAttack) this.specialAttackCounter++;
             this.attackHero()
+            this.healCount--;
         },
         stun() {
-            const stun = getRandomInRange(1, 2)
+            const stun = getRandomNumberBetween(1, 2)
             if (stun === 2) {
                 this.monsterStunnedCountDown = 2
-                this.addLog('Monster', 'stun', `successfully stunned Monster for ${this.monsterStunnedCountDown} turn(s)`)
+                this.addLog(this.currentMonster.name, 'stun', `successfully stunned Monster for ${this.monsterStunnedCountDown} turn(s)`)
             } else {
-                this.addLog('Monster', 'stunFail', 'attempted to stun Monster and failed')
+                this.addLog(this.currentMonster.name, 'stunFail', 'attempted to stun Monster and failed')
                 this.attackHero()
             }
-            if (this.usedSpecialAttack) this.currentRound++;
+            if (this.usedSpecialAttack) this.specialAttackCounter++;
         },
         reset() {
             this.hero.heroHp = this.hero.maxHp;
+            this.currentMonster = this.goblin;
             this.goblin.monsterHp = this.goblin.maxHp;
-            this.goblin.stun = false;
-            this.currentRound = 0;
+            this.barbarian.monsterHp = this.barbarian.maxHp;
+            this.dracula.monsterHp = this.dracula.maxHp;
+            this.barbarian.strength = this.barbarian.baseStrength;
+            this.specialAttackCounter = 0;
             this.usedSpecialAttack = false;
-            this.monsterAttackStyle = 'ranged';
-            this.monsterAttackStyleOrder = ['ranged', 'melee', 'melee', 'mage'],
+            this.currentMonster.monsterAttackStyle = '';
+            this.currentMonster.monsterAttackStyleOrder = this.goblin.monsterAttackStyleOrder
             this.battleLogs = [];
+            this.currentRound = 1
+            this.specialAttackMinHit = 16
+            this.specialAttackMaxHit = 24
+            this.healCount = 10
         },
         surrender() {
             this.hero.heroHp = 0;
@@ -151,26 +227,30 @@ const app = Vue.createApp({
                 message, 
                 amount
             });
-            this.idCounter++;
+        },
+        calculateBarbarianStrength() {
+            const healthPointsMissing = this.barbarian.maxHp - this.barbarian.monsterHp
+            const newStrength = Math.ceil((healthPointsMissing / 100 + 1) * this.barbarian.baseStrength)
+            this.barbarian.strength = newStrength
         }
     },
     computed: {
         monsterHealthBarValue() {
-            if (this.goblin.monsterHp < 0) return 0
-            else if (this.goblin.monsterHp === this.goblin.maxHp) return this.goblin.maxHp
-            return Math.floor(this.goblin.monsterHp / this.goblin.maxHp * this.goblin.maxHp)
+            if (this.currentMonster.monsterHp < 0) return 0
+            else if (this.currentMonster.monsterHp === this.currentMonster.maxHp) return this.currentMonster.maxHp
+            return round(this.currentMonster.monsterHp / this.currentMonster.maxHp * this.currentMonster.maxHp)
         },
         heroHealthBarValue() {
             if (this.hero.heroHp < 0) return 0
             else if (this.hero.heroHp === this.hero.maxHp) return this.hero.maxHp
-            return Math.floor(this.hero.heroHp / this.hero.maxHp * this.hero.maxHp)
+            return round(this.hero.heroHp / this.hero.maxHp * this.hero.maxHp)
         }, 
         monsterHealthBarStyle() {
-            if (this.goblin.monsterHp < 0) {
+            if (this.currentMonster.monsterHp < 0) {
                 return {width: '0%'}
             }
             return { 
-                width: `${Math.floor(this.goblin.monsterHp / this.goblin.maxHp * 100)}%`
+                width: `${Math.round(this.currentMonster.monsterHp / this.currentMonster.maxHp * 100)}%`
             }
         },
         heroHealthBarStyle() {
@@ -182,16 +262,23 @@ const app = Vue.createApp({
             }
         }, 
         specialAttackOnCooldown() {
-            if (this.currentRound % 3 === 0) this.usedSpecialAttack = false
+            if (this.specialAttackCounter % 3 === 0) this.usedSpecialAttack = false
             return this.usedSpecialAttack;
         },
         gameOver() {
-            return this.hero.heroHp <= 0 || this.goblin.monsterHp <= 0
+            let gameOver = false
+            if (this.hero.heroHp <= 0) {
+                gameOver = true
+            }
+            if (this.currentRound === 4 && this.currentMonster.monsterHp <= 0) {
+                gameOver = true
+            }
+            return gameOver
         },
         monsterIcon() {
-            if (this.monsterAttackStyle === 'ranged') return 'assets/bowman.png'
-            else if (this.monsterAttackStyle === 'mage') return 'assets/lunar-wand.png'
-            else if (this.monsterAttackStyle === 'melee') return 'assets/swordman.png'
+            if (this.currentMonster.monsterAttackStyle === 'ranged') return 'assets/bowman.png'
+            else if (this.currentMonster.monsterAttackStyle === 'mage') return 'assets/lunar-wand.png'
+            else if (this.currentMonster.monsterAttackStyle === 'melee') return 'assets/swordman.png'
             else return 'assets/uncertainty.png'
         },
         heroIcon() {
@@ -202,13 +289,17 @@ const app = Vue.createApp({
         }
     },
     mounted: function () {
+        this.currentMonster = this.goblin
+        this.currentMonster.monsterAttackStyleOrder = this.currentMonster.monsterAttackStyleOrder
         // Heal Monster if his heal is low enough
         window.setInterval(() => {
-            if (this.goblin.monsterHp > 0 && this.goblin.monsterHp <= Math.floor(this.goblin.maxHp / 4)) {
-                if (this.goblin.monsterHp + 1 > this.goblin.maxHp) {
-                    this.goblin.monsterHp = this.goblin.maxHp
-                } else {
-                    this.goblin.monsterHp += 1
+            if (this.currentMonster.name === 'Dracula') {
+                if (this.currentMonster.monsterHp > 0 && this.currentMonster.monsterHp <= round(this.currentMonster.maxHp / 4)) {
+                    if (this.currentMonster.monsterHp + 2 > this.currentMonster.maxHp) {
+                        this.currentMonster.monsterHp = this.currentMonster.maxHp
+                    } else {
+                        this.currentMonster.monsterHp += 2
+                    }
                 }
             }
         }, 500)
